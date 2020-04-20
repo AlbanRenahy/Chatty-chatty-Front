65dc16c939d5e01a8fabeba4834df539fb403360
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-20 11:19:36 +0200

    Redux setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// == Import : npm
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+// == Import : local
+import App from '../src/components/App';
+import store from '../src/store';
+import './index.css';
+
+// == Render
+const rootComponent = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// React render into the DOM
+render(rootComponent, document.getElementById('root'));
