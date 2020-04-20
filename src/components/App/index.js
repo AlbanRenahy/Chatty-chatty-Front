43429e84c0f8780a46c6fc26abf37bebed2962df
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-20 15:45:32 +0200

    ChatList component ready + first integration

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,16 +7,18 @@ import './app.scss';
 // == Import : style
 
 // Import locaux
-//import Nav from '../../components/Nav';
-import HomePage from '../../components/HomePage';
+import Nav from '../../components/Nav';
+//import HomePage from '../../components/HomePage';
 //import UserProfil from '../../components/UserProfil';
+import ChatList from '../ChatList';
+import MessagesList from '../ChatList/MessagesList';
 
 // == Composant
 const App = () => (
   <div className="app">
-    {/* <Nav /> */}
-    <HomePage />
-    {/* <UserProfil /> */}
+    <Nav />
+    <ChatList />
+    <MessagesList />
   </div>
 );
 
---
+++ src/components/ChatList/MessagesList.js
@@ -0,0 +1,28 @@
+// == Import : npm
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+// import { NavLink } from 'react-router-dom';
+
+ // == Import : local
+ import MessagePreview from './MessagePreview';
+
+ // == Import : style
+ import './scrollinglist.scss';
+
+
+ // == Composant
+const MessagesList = () => (
+    <div className="messages-list">
+    <MessagePreview />
+    <MessagePreview />
+    <MessagePreview />
+    <MessagePreview />
+    <MessagePreview />
+    <MessagePreview />
+    <MessagePreview />
+    <MessagePreview />
+  </div>
+);
+
+ // == Export
+export default MessagesList;
